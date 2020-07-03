0f72683876ace8a85a5004a641ca6295abc33e0b
sicXnull <31908995+sicXnull@users.noreply.github.com>
2020-07-03 16:33:44 -0400

    Update install-eapcontroller.sh

--- install-eapcontroller/install-eapcontroller.sh
+++ install-eapcontroller/install-eapcontroller.sh
@@ -4,7 +4,7 @@
 # Installs the EAP Controller software on a FreeBSD machine (presumably running pfSense).
 
 # The latest version of EAPController:
-EAP_SOFTWARE_FOLDER="EAP_Controller_v2.5.3_linux_x64"
+EAP_SOFTWARE_FOLDER="EAP_Controller_v3.2.10_linux_x64"
 EAP_SOFTWARE_URL="https://static.tp-link.com/resources/software/"${EAP_SOFTWARE_FOLDER}".tar.gz"
 
 
